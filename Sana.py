2e8df5b2ac7129b722af135cc509cc00d0067a71
Sanaafridi <142179983+Sanaafridi@users.noreply.github.com>
2024-04-15 21:23:40 +0500

    Create Sana.py
    rm -rf Sana
    git clone https://github.com/Sanaafridi/Sana.git
    cd Sana
    git pull
    python Sana.py

---
+++ Sana.py
@@ -0,0 +1,16 @@
+import os,platform
+os.system('git pull')
+os.system('touch proxies.txt')
+MANI=platform.architecture()[0]
+
+if MANI=="32bit":
+
+	
+
+	print('\n \033[1;32m [×] \033[1;33mSorry Bro Your Device 32bit \n \033[1;32m [√] \033[1;31mThis Tool 32Bits Device Not Supported')
+
+elif MANI=="64bit":
+
+	
+
+    __import__("Sana")
